3bf153b72d5539ed237e101318c50e4d013a3604
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2025-03-04 15:10:04 +0800

    some change with login component subfiles

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  onButtonClick() {
+    console.log("Button was clicked");
+   }
+
+}
