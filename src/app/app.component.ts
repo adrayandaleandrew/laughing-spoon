49bd794e2ce731bd5ea218f9a11faeab8fa3e082
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2025-03-06 10:00:26 +0300

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,LoginComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+   <app-header />  
+    <h1>Welcome to {{title()}}!</h1>
+    <app-login />
     <router-outlet />
   `,
+  
   styles: [],
 })
 export class AppComponent {
-  title = 'app-burner';
+  title = signal('app');
 }
