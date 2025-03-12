98e6b4d4543e94c6b40dbf3445570bc804f94747
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2025-03-12 10:23:20 +0300

    add function to change ui, import FormsModule in the component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  onButtonClick() {
-   }
+  userName: any = '';
+  userPassword: string = '';
+  userEmail: string = '';
+  
+  isLogin: boolean = true;
 
+  openLogin(event: any) {
+    if(this.isLogin == false){
+      event.preventDefault();
+      this.isLogin = true;
+    }else{
+      event.preventDefault();
+      this.isLogin = false;
+    }
+  }
+
+  onSubmit(event: any) {
+    
+    if(this.isLogin == false){
+      console.log(this.userEmail);
+      console.log(this.userName);
+      console.log(this.userPassword);
+    }else{
+      console.log(this.userName);
+      console.log(this.userPassword);
+    }
+  }
+
+  
+   
 }
