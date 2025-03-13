835c906e2c463a1f398510808c2eb5fd3d2f65f5
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2025-03-13 12:58:41 +0300

    minor changes

--- .angular/cache/19.2.0/app-burner/vite/deps/@angular_platform-browser.js
+++ .angular/cache/19.2.0/app-burner/vite/deps/@angular_platform-browser.js
@@ -27,7 +27,6 @@ import {
   createApplication,
   disableDebugTools,
   enableDebugTools,
-  getDOM,
   initDomAdapter,
   platformBrowser,
   provideClientHydration,
@@ -37,8 +36,11 @@ import {
   withI18nSupport,
   withIncrementalHydration,
   withNoHttpTransferCache
-} from "./chunk-PWVNXA7C.js";
-import "./chunk-MLSFSH6F.js";
+} from "./chunk-D7YLUVFH.js";
+import {
+  getDOM
+} from "./chunk-OO6BS24S.js";
+import "./chunk-TXZMGFPN.js";
 export {
   BrowserModule,
   By,
